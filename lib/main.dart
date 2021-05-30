2e38c67f834a56fa2cd680e6a7d707781faf567e
PedroCones <pedrocones@yahoo.com>
2021-05-30 12:46:10 -0600

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
 
+
 void main() {
   runApp(MyApp());
 }
+
+
+//new test file to develop action log
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
